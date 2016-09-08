4ada81cda14e013d6fa79e1466668e1570eace35
wxb-bimt <wxb@bimt.com>
2016-09-08 09:55:11 +0800

    a

--- myIonic/www/tutorial/ion-checkbox/app.js
+++ myIonic/www/tutorial/ion-checkbox/app.js
@@ -1,48 +1,18 @@
 ﻿angular.module('ionicApp', ['ionic'])
 
-  .controller('MyCtrl', function($scope, $ionicListDelegate) {
+  .controller('MainCtrl', function($scope) {
 
-    $scope.data = {
-      showDelete: false
-    };
-
-    $scope.showDeleteButtons = function() {
-      $scope.data.showDelete=!$scope.data.showDelete;
-      $ionicListDelegate.showDelete($scope.data.showDelete);
-    };
-
-    $scope.edit = function(item) {
-      alert('Edit Item: ' + item.id);
-    };
-    $scope.share = function(item) {
-      alert('Share Item: ' + item.id);
-    };
-    $scope.top=function(item, fromIndex){
-      alert('Top Item: ' + item.id);
-    };
-    $scope.moveItem = function(item, fromIndex, toIndex) {
-
-      $scope.items.splice(fromIndex, 1);
-      $scope.items.splice(toIndex, 0, item);
-    };
-
-    $scope.onItemDelete = function(item) {
-      $scope.items.splice($scope.items.indexOf(item), 1);
-    };
-
-    $scope.items = [
-      { id: 0 },
-      { id: 1 },
-      { id: 2 },
-      { id: 3 },
-      { id: 4 },
-      { id: 5 },
-      { id: 6 },
-      { id: 7 },
-      { id: 8 },
-      { id: 9 },
-      { id: 10 }
-
+    $scope.devList = [
+      { text: "HTML5", checked: true },
+      { text: "CSS3", checked: false },
+      { text: "JavaScript", checked: false }
     ];
+
+    $scope.pushNotificationChange = function() {
+      console.log('Push Notification Change', $scope.pushNotification.checked);
+    };
+
+    $scope.pushNotification = { checked: true };
+    $scope.emailNotification = 'Subscribed';
 
   });
